9d4d415a8eff3d2e1d04cd1bd77d95dd59ef60d0
Martin Kurbel <martin@kurbel.sk>
2020-05-07 20:37:27 +0200

    Initial Commit

---
+++ Kiwi.com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kiwi.com
+//
+//  Created by Martin Kurbel on 07/05/2020.
+//  Copyright © 2020 Martin Kurbel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

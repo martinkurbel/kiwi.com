7871d56e51c376cf00bea53906bf614a29c28008
Martin Kurbel <martin@kurbel.sk>
2020-05-12 16:41:35 +0000

    Update UI in cell

--- Kiwi.com/Views/Cells/FlightOfferCell.swift
+++ Kiwi.com/Views/Cells/FlightOfferCell.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class FlightOfferCell: UICollectionViewCell {
     
-    private let coverImage = UIImageView()
-    private let titleLabel = KILabel(size: 20)
+    struct Sizes {
+        static let offset: CGFloat = 16
+    }
+    
+    let coverImage = UIImageView()
+    private let titleLabel = KILabel(style: .semibold ,size: 22)
+    private let priceLabel = KILabel(style: .semibold, size: 22, align: .right)
     private let descriptionLabel = KILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
         
         coverImage.contentMode = .scaleToFill
         coverImage.backgroundColor = .yellow
@@ -29,9 +43,34 @@ class FlightOfferCell: UICollectionViewCell {
             coverImage.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1/2)
         ])
         
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        titleLabel.text = "Berlin"
+        
+        self.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: Sizes.offset),
+            titleLabel.topAnchor.constraint(equalTo: coverImage.bottomAnchor, constant: 32)
+        ])
+        
+        priceLabel.text = "100.00€"
+        
+        self.addSubview(priceLabel)
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            priceLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 88),
+            priceLabel.leftAnchor.constraint(equalTo: titleLabel.rightAnchor, constant: Sizes.offset),
+            priceLabel.topAnchor.constraint(equalTo: coverImage.bottomAnchor, constant: 32),
+            priceLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -Sizes.offset),
+        ])
+        
+        descriptionLabel.text = "Aasaf asfjaf asfjajsf ajf ajsf ajfsf afj asfja fs"
+        
+        self.addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: Sizes.offset),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Sizes.offset),
+            descriptionLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -Sizes.offset),
+        ])
     }
 }
